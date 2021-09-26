155bbab435cd795930f75e86113c5d8d2c57a951
Mohamed Hani <mohamedhani01014@gmail.com>
2021-09-26 16:26:09 +0200

    fix issue on node template

--- modules/eks/node_template.tf
+++ modules/eks/node_template.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_template" "worker_node_template" {
     name = "worker_node_template"
     key_name = "${var.project_name}-key"
-    vpc_security_group_ids = [aws_security_group.default_node_group_sg.id , aws_security_group.node_worker_group_sg.id]
+    vpc_security_group_ids = [aws_security_group.node_group_lb_sg.id , aws_security_group.node_worker_group_sg.id]
 
      block_device_mappings {
     device_name = "/dev/sda1"
